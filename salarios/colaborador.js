f5aff3b7e24e28004700ce2aef7c62f78dc23b3d
Uiara Dantas <73133103+kahdantas@users.noreply.github.com>
2021-04-14 14:06:34 -0300

    salarios-teste3

--- salarios/colaborador.js
+++ salarios/colaborador.js
@@ -3,4 +3,4 @@ let valor2 = parseInt(gets());
 let valor3 = parseFloat(gets());
 let salary = parseFloat(valor2 * valor3).toFixed(2); // Digite aqui o calculo do salário
 console.log("NUMBER = " + valor1);
-console.log("SALARY = U$ " + salary);
+console.log("SALARY = U$ " + salary); 
